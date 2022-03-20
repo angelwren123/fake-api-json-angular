752f32ebc0e212cc9a07055f22b2bdf1683baf6d
angelwren123 <84611289+angelwren123@users.noreply.github.com>
2022-03-20 14:47:10 +0700

    initial commit

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+// const tags = require("./data/tags");
+// const brands = require("./data/brands");
+// const itemTypes = require("./data/itemTypes");
 
 module.exports = {
   items,
-  tags,
-  brands,
-  itemTypes
+  // tags,
+  // brands,
+  // itemTypes
 }
 
 
